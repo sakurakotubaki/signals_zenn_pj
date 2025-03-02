63db8e735ca2f682cad362eac468729e382da415
sakurakotubaki <gitonly543@gmail.com>
2025-03-02 14:23:11 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# signals_zenn_pj
+zennで紹介するsignalsのサンプルコード
+
+## 2025/03/02 (SUN)
+- flutter "3.29.0"
+- dart 3.7.0
---
+++ android/app/src/main/kotlin/com/example/signals_zenn_pj/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.signals_zenn_pj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
